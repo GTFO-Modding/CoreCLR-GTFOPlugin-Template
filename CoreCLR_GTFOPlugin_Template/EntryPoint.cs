ab130c3795f711527dd1fa931550a1f32d18cc4e
Flowaria <wnsghgj1234@naver.com>
2022-07-20 09:37:45 +0900

    Initial

---
+++ CoreCLR_GTFOPlugin_Template/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using System.Linq;
+
+namespace TemplatePlugin
+{
+    [BepInPlugin("TemplatePlugin.GUID", VersionInfo.RootNamespace, VersionInfo.Version)]
+    internal class EntryPoint : BasePlugin
+    {
+        private Harmony _Harmony;
+
+        public override void Load()
+        {
+            _Harmony = new Harmony($"{VersionInfo.RootNamespace}.Harmony");
+            _Harmony.PatchAll();
+
+            Logger.Info($"Plugin has loaded with {_Harmony.GetPatchedMethods().Count()} patches!");
+        }
+    }
+}
